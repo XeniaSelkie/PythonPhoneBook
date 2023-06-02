5b897d0077cdd035fdb0ab6b383539b8deecf42c
Sana <mypostoffice94@gmail.com>
2023-06-02 22:33:20 +0300

    Add lecture

---
+++ functional.py
@@ -0,0 +1,35 @@
+#def add_cont(file_name, str):
+
+def create(path):
+    try:
+        file = open(path, 'r')
+    except IOError:
+        print('Создан новый справочник -> phone_book.txt ')
+        file = open(path, 'w')
+    finally:
+        file.close()    
+
+def add_cont(file_name, fio):
+    data = open(file_name, 'a')
+    data.write(fio + '\n')
+    data.close()
+
+def show_all(file_name):
+    data = open(file_name, 'r')
+    for line in data:
+        print(line[:-2])
+    data.close()    
+    
+def search(file_name, stroka):
+    vyhod = 0
+    data = open(file_name, 'r')
+    for line in data:
+        if str in line:
+            print(line)
+            vyhod = 123
+            break
+    if vyhod != 123:    
+        print('Такого контакта нет')    
+    data.close()
+
+ 
